ba3a78a866a26c43b1697ee287775a75ae207992
Alex Kuvaldin <kuvaldin.ai95@gmail.com>
2023-04-03 18:25:02 +0700

    removing redundant nesting

--- local/templates/.default/components/bitrix/news.list/template2/template.php
+++ local/templates/.default/components/bitrix/news.list/template2/template.php
@@ -13,36 +13,34 @@
 $this->setFrameMode(true);
 ?>
 
-<div class="news-list">
-  <?php foreach ($arResult["ITEMS"] as $key => $arItem) {
-        $unixDate = MakeTimeStamp($arItem["TIMESTAMP_X"], "DD.MM.YYYY HH:MI:SS");
+<?php foreach ($arResult["ITEMS"] as $key => $arItem) {
+    $unixDate = MakeTimeStamp($arItem["TIMESTAMP_X"], "DD.MM.YYYY HH:MI:SS");
 
-        if ($key === 0) { ?>
-          <article class="news-important" style="background-image: url(<?php echo $arItem["PREVIEW_PICTURE"]["SRC"]; ?>)">
-            <a href="/article.html" class="news-important__link">
-              <h2 class="news-important__title">
-                  <?php echo !empty($arItem["PREVIEW_TEXT"]) ? $arItem["PREVIEW_TEXT"] : $arItem["NAME"] ?>
-              </h2>
-            </a>
-            <time class="news-important__publication-date" datetime="<?php echo FormatDate('Y-m-d', $unixDate) ?>">
-                <?php echo FormatDate('j F Y', $unixDate) ?>
-            </time>
-          </article>
-        <?php } else { ?>
-          <article class="news news--wide">
-            <div class="news__publication-info">
-              <a href="/article.html" class="news__link content-block">
-                <h3 class="news__title content-block">
-                    <?php echo !empty($arItem["PREVIEW_TEXT"]) ? $arItem["PREVIEW_TEXT"] : $arItem["NAME"] ?>
-                </h3>
-              </a>
-              <time class="news__publication-date" datetime="<?php echo FormatDate('Y-m-d', $unixDate) ?>">
-                  <?php echo FormatDate('j F Y', $unixDate) ?>
-              </time>
-            </div>
-            <div class="news__illustration"
-                 style="background-image: url(<?php echo $arItem["PREVIEW_PICTURE"]["SRC"]; ?>)"></div>
-          </article>
-        <?php } ?>
+    if ($key === 0) { ?>
+      <article class="news-important" style="background-image: url(<?php echo $arItem["PREVIEW_PICTURE"]["SRC"]; ?>)">
+        <a href="/article.html" class="news-important__link">
+          <h2 class="news-important__title">
+              <?php echo !empty($arItem["PREVIEW_TEXT"]) ? $arItem["PREVIEW_TEXT"] : $arItem["NAME"] ?>
+          </h2>
+        </a>
+        <time class="news-important__publication-date" datetime="<?php echo FormatDate('Y-m-d', $unixDate) ?>">
+            <?php echo FormatDate('j F Y', $unixDate) ?>
+        </time>
+      </article>
+    <?php } else { ?>
+      <article class="news news--wide">
+        <div class="news__publication-info">
+          <a href="/article.html" class="news__link content-block">
+            <h3 class="news__title content-block">
+                <?php echo !empty($arItem["PREVIEW_TEXT"]) ? $arItem["PREVIEW_TEXT"] : $arItem["NAME"] ?>
+            </h3>
+          </a>
+          <time class="news__publication-date" datetime="<?php echo FormatDate('Y-m-d', $unixDate) ?>">
+              <?php echo FormatDate('j F Y', $unixDate) ?>
+          </time>
+        </div>
+        <div class="news__illustration"
+             style="background-image: url(<?php echo $arItem["PREVIEW_PICTURE"]["SRC"]; ?>)"></div>
+      </article>
     <?php } ?>
-</div>
+<?php } ?>
