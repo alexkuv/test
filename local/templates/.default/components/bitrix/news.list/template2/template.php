66fd1dc404991d4e1b64fd69bcbab3bb74fc0be9
Alex Kuvaldin <kuvaldin.ai95@gmail.com>
2023-04-04 15:04:50 +0700

    correct date, add link to detail page

--- local/templates/.default/components/bitrix/news.list/template2/template.php
+++ local/templates/.default/components/bitrix/news.list/template2/template.php
@@ -14,29 +14,27 @@ $this->setFrameMode(true);
 ?>
 
 <?php foreach ($arResult["ITEMS"] as $key => $arItem) {
-    $unixDate = MakeTimeStamp($arItem["TIMESTAMP_X"], "DD.MM.YYYY HH:MI:SS");
-
     if ($key === 0) { ?>
       <article class="news-important" style="background-image: url(<?php echo $arItem["PREVIEW_PICTURE"]["SRC"]; ?>)">
-        <a href="/article.html" class="news-important__link">
+        <a href="<?php echo $arItem["DETAIL_PAGE_URL"] ?>" class="news-important__link">
           <h2 class="news-important__title">
               <?php echo !empty($arItem["PREVIEW_TEXT"]) ? $arItem["PREVIEW_TEXT"] : $arItem["NAME"] ?>
           </h2>
         </a>
-        <time class="news-important__publication-date" datetime="<?php echo FormatDate('Y-m-d', $unixDate) ?>">
-            <?php echo FormatDate('j F Y', $unixDate) ?>
+        <time class="news-important__publication-date" datetime="<?php echo FormatDateFromDB($arItem["TIMESTAMP_X"], "YYYY-MM-DD") ?>">
+            <?php echo FormatDateFromDB($arItem["TIMESTAMP_X"], "DD MMMM YYYY") ?>
         </time>
       </article>
     <?php } else { ?>
       <article class="news news--wide">
         <div class="news__publication-info">
-          <a href="/article.html" class="news__link content-block">
+          <a href="<?php echo $arItem["DETAIL_PAGE_URL"] ?>" class="news__link content-block">
             <h3 class="news__title content-block">
                 <?php echo !empty($arItem["PREVIEW_TEXT"]) ? $arItem["PREVIEW_TEXT"] : $arItem["NAME"] ?>
             </h3>
           </a>
-          <time class="news__publication-date" datetime="<?php echo FormatDate('Y-m-d', $unixDate) ?>">
-              <?php echo FormatDate('j F Y', $unixDate) ?>
+          <time class="news__publication-date" datetime="<?php echo FormatDateFromDB($arItem["TIMESTAMP_X"], "YYYY-MM-DD") ?>">
+              <?php echo FormatDateFromDB($arItem["TIMESTAMP_X"], "DD MMMM YYYY") ?>
           </time>
         </div>
         <div class="news__illustration"
